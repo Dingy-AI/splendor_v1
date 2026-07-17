d17073f7b591c1375bcc0024e03b1a3695af1dbe
Ding <dingkzhang@gmail.com>
2026-07-17 11:12:53 -0500

    adding raise value error and other test cases

--- tests/test_02_action_mapping.py
+++ tests/test_02_action_mapping.py
@@ -7,7 +7,7 @@ from splendor_v1.env.core.card import Card
 def env():
     return SplendorEnv()
 
-def test_action_to_id(env):
+def test_action_to_id_round_trip(env):
 
     env.reset()
     actions = env._legal_actions(env.state)
@@ -17,8 +17,19 @@ def test_action_to_id(env):
 
         action_id = env.action_to_id(action)
         recovered_action = env.id_to_action(action_id)
-        print("action id:", action_id)
-
-        print("action:", action)
-        print("recovered action:", recovered_action)
         assert action == recovered_action
+
+def test_id_to_action_round_trip(env):
+    env.reset()
+    
+    action = env.id_to_action(5)
+
+    recovered = env.action_to_id(action)
+
+    assert 5 == recovered 
+
+def test_invalid_action_id(env):
+    env.reset()
+    with pytest.raises(ValueError):
+        env.id_to_action(-1)
+
