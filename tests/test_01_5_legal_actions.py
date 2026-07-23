d7a7ddc19b9f759fa5e1d226fc36863562167032
Ding <dingkzhang@gmail.com>
2026-07-23 17:02:54 -0500

    testing legal buy actions

--- tests/test_01_5_legal_actions.py
+++ tests/test_01_5_legal_actions.py
@@ -2,14 +2,12 @@ import pytest
 from splendor_v1.env.env import SplendorEnv
 from splendor_v1.env.core.enums import GemColor, NodeType, ActionType, GemColor
 from splendor_v1.env.core.card import Card
-from splendor_v1.env.core.actions import Action
-from splendor_v1.env.core.action_constants import ACTION_SPACE_SIZE, DISCARD_START, NOBLE_START
-from copy import deepcopy
+
 @pytest.fixture
 def env():
     return SplendorEnv()
 
-def test_buy_visible_card(env):
+def test_legal_actions_count(env):
     env.reset()
 
     env.state.bank = {
@@ -18,9 +16,8 @@ def test_buy_visible_card(env):
         GemColor.GREEN: 3,
         GemColor.RED: 3,
         GemColor.BLACK: 4,
-        GemColor.GOLD: 5
+        GemColor.GOLD: 4
     }
-
 
     env.state.visible_cards[0][0] = Card(
         id=500,
@@ -42,28 +39,30 @@ def test_buy_visible_card(env):
             GemColor.RED: 1,
             GemColor.GREEN: 1,
             GemColor.BLACK: 0,
-            GemColor.GOLD: 0
+            GemColor.GOLD: 1
     }
 
+    actions = env._legal_actions(env.state)
 
-    action = Action(
-        action_type = ActionType.BUY_VISIBLE,
-        tier=0,
-        slot=0,
-        payment_id=0,
-        gold_payment=(0,0,0,0,0)
-    )
-
-    env.step(action)
+    print(len(actions))
 
 
-    assert sum(env.state.players[0].gems.values()) == 0
-    assert env.state.players[0].bonuses[GemColor.RED] == 1
-    assert env.state.players[0].points == 2
+    take_gems = 0
+    discard_gems = 0
+    actions_list = []
+    for action in actions:
 
-    assert sum(env.state.bank.values()) == 25
-    assert env.state.visible_cards[0][0].id != 500
-    #TODO running into an issue with buying a card. 
-    # you have to also 'choose' which gems you want to get rid depending on if you have gold or not
-    
-    assert env.state.players[0].purchased_cards[0].id == 500
+        if action.action_type == ActionType.TAKE_GEMS:
+            take_gems += 1
+        if action.action_type == ActionType.DISCARD_GEMS:
+            discard_gems += 1
+
+        if action.action_type == ActionType.BUY_VISIBLE and action.tier == 0 and action.slot == 0:
+            actions_list.append(action)
+
+
+    assert take_gems == 26
+    assert discard_gems == 0
+
+    print(actions_list)
+    print(len(actions_list))
