1619c00972ad4b5046fbae7e36430826e6415700
Ding <dingkzhang@gmail.com>
2026-07-23 19:44:04 -0500

    tie player test

--- tests/test_06_buy.py
+++ tests/test_06_buy.py
@@ -172,3 +172,199 @@ def test_end_game_trigger_second_player(env):
     assert len(env.state.winners) == 1
     assert env.state.winners[0] == 1
 
+def test_end_game_first_trigger_second_end_tie(env):
+    env.reset()
+
+    env.state.bank = {
+        GemColor.WHITE: 3,
+        GemColor.BLUE: 3,
+        GemColor.GREEN: 3,
+        GemColor.RED: 3,
+        GemColor.BLACK: 4,
+        GemColor.GOLD: 5
+    }
+
+
+    env.state.visible_cards[1][0] = Card(
+        id=500,
+        tier=1,
+        points=1,
+        bonus_color=GemColor.RED,
+        cost={ 
+            GemColor.WHITE: 1,
+            GemColor.BLUE: 1,
+            GemColor.RED: 1,
+            GemColor.GREEN: 1,
+            GemColor.BLACK:0
+        }
+    )
+
+    env.state.players[0].gems ={ 
+            GemColor.WHITE: 1,
+            GemColor.BLUE: 1,
+            GemColor.RED: 1,
+            GemColor.GREEN: 1,
+            GemColor.BLACK: 0,
+            GemColor.GOLD: 0
+    }
+
+    env.state.players[0].points = 14
+    env.state.players[1].points = 14
+    action = Action(
+        action_type = ActionType.BUY_VISIBLE,
+        tier=1,
+        slot=0,
+        payment_id=0,
+        gold_payment=(0,0,0,0,0)
+    )
+
+    assert len(env.state.winners) == 0
+    obs, reward, terminated, truncated, info = env.step(action)
+    assert len(env.state.winners) == 0
+    assert env.state.winners == []
+
+    env.state.bank = {
+        GemColor.WHITE: 3,
+        GemColor.BLUE: 3,
+        GemColor.GREEN: 3,
+        GemColor.RED: 3,
+        GemColor.BLACK: 4,
+        GemColor.GOLD: 5
+    }
+
+
+    env.state.visible_cards[1][0] = Card(
+        id=500,
+        tier=1,
+        points=1,
+        bonus_color=GemColor.RED,
+        cost={ 
+            GemColor.WHITE: 1,
+            GemColor.BLUE: 1,
+            GemColor.RED: 1,
+            GemColor.GREEN: 1,
+            GemColor.BLACK:0
+        }
+    )
+
+    env.state.players[1].gems ={ 
+            GemColor.WHITE: 1,
+            GemColor.BLUE: 1,
+            GemColor.RED: 1,
+            GemColor.GREEN: 1,
+            GemColor.BLACK: 0,
+            GemColor.GOLD: 0
+    }
+
+    action = Action(
+        action_type = ActionType.BUY_VISIBLE,
+        tier=1,
+        slot=0,
+        payment_id=0,
+        gold_payment=(0,0,0,0,0)
+    )
+
+    assert len(env.state.winners) == 0
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    assert len(env.state.winners) == 2
+    assert env.state.winners[0] == 0
+    assert env.state.winners[1] == 1    
+
+def test_end_game_first_trigger_second_end_tie(env):
+    env.reset()
+
+    env.state.bank = {
+        GemColor.WHITE: 3,
+        GemColor.BLUE: 3,
+        GemColor.GREEN: 3,
+        GemColor.RED: 3,
+        GemColor.BLACK: 4,
+        GemColor.GOLD: 5
+    }
+
+
+    env.state.visible_cards[1][0] = Card(
+        id=500,
+        tier=1,
+        points=1,
+        bonus_color=GemColor.RED,
+        cost={ 
+            GemColor.WHITE: 1,
+            GemColor.BLUE: 1,
+            GemColor.RED: 1,
+            GemColor.GREEN: 1,
+            GemColor.BLACK:0
+        }
+    )
+
+    env.state.players[0].gems ={ 
+            GemColor.WHITE: 1,
+            GemColor.BLUE: 1,
+            GemColor.RED: 1,
+            GemColor.GREEN: 1,
+            GemColor.BLACK: 0,
+            GemColor.GOLD: 0
+    }
+
+    env.state.players[0].points = 14
+    env.state.players[1].points = 14
+    action = Action(
+        action_type = ActionType.BUY_VISIBLE,
+        tier=1,
+        slot=0,
+        payment_id=0,
+        gold_payment=(0,0,0,0,0)
+    )
+
+    assert len(env.state.winners) == 0
+    obs, reward, terminated, truncated, info = env.step(action)
+    assert len(env.state.winners) == 0
+    assert env.state.winners == []
+
+    env.state.bank = {
+        GemColor.WHITE: 3,
+        GemColor.BLUE: 3,
+        GemColor.GREEN: 3,
+        GemColor.RED: 3,
+        GemColor.BLACK: 4,
+        GemColor.GOLD: 5
+    }
+
+
+    env.state.visible_cards[1][0] = Card(
+        id=500,
+        tier=1,
+        points=2,
+        bonus_color=GemColor.RED,
+        cost={ 
+            GemColor.WHITE: 1,
+            GemColor.BLUE: 1,
+            GemColor.RED: 1,
+            GemColor.GREEN: 1,
+            GemColor.BLACK:0
+        }
+    )
+
+    env.state.players[1].gems ={ 
+            GemColor.WHITE: 1,
+            GemColor.BLUE: 1,
+            GemColor.RED: 1,
+            GemColor.GREEN: 1,
+            GemColor.BLACK: 0,
+            GemColor.GOLD: 0
+    }
+
+    action = Action(
+        action_type = ActionType.BUY_VISIBLE,
+        tier=1,
+        slot=0,
+        payment_id=0,
+        gold_payment=(0,0,0,0,0)
+    )
+
+    assert len(env.state.winners) == 0
+    obs, reward, terminated, truncated, info = env.step(action)
+
+    assert len(env.state.winners) == 1
+    assert env.state.winners[0] == 1    
