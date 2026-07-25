0fca0f1d9fa5e0d5ee470e2baa5e3e8f4a0dae11
Ding <dingkzhang@gmail.com>
2026-07-24 21:53:09 -0500

    adding discard crazy test case

--- tests/test_08_discard.py
+++ tests/test_08_discard.py
@@ -68,6 +68,81 @@ def test_discard(env):
     assert env.state.node_type == NodeType.MAIN_DECISION
     assert env.state.current_player == 1
 
+def test_discard_noble(env):
+    env.reset()
+
+    env.state.players[0].gems = {
+        GemColor.WHITE: 3,
+        GemColor.BLACK: 0,
+        GemColor.GREEN: 3,
+        GemColor.BLUE: 0,
+        GemColor.RED: 0,
+        GemColor.GOLD: 3
+    }
+
+    env.state.players[0].bonuses = {
+        GemColor.WHITE: 4,
+        GemColor.BLACK: 4,
+        GemColor.GREEN: 4,
+        GemColor.BLUE: 4,
+        GemColor.RED: 4,
+    }
+
+    action = Action (
+
+        action_type=ActionType.TAKE_GEMS,
+        gem_colors=(GemColor.GREEN, GemColor.BLUE, GemColor.RED)
+    )
+
+    env.step(action)
+
+    assert env.state.node_type == NodeType.OVERFLOW_DISCARD
+    assert env.state.current_player == 0
+
+    actions = env._legal_actions(env.state)
+
+    env.step(actions[0])
+    assert env.state.node_type == NodeType.OVERFLOW_DISCARD
+    assert env.state.current_player == 0
+
+    env.step(actions[0])
+
+    assert env.state.node_type == NodeType.NOBLE_CLAIM
+    assert env.state.current_player == 0
+
+    actions = env._legal_actions(env.state)
+
+    env.step(actions[0])
+
+    assert env.state.node_type == NodeType.MAIN_DECISION
+    assert env.state.current_player == 1
+
+
+    actions = env._legal_actions(env.state)
+
+    env.step(actions[0])
+    assert env.state.node_type == NodeType.MAIN_DECISION
+    assert env.state.current_player == 0
+
+    #reserve action i think
+    env.step(actions[0])
+
+
+    assert env.state.node_type == NodeType.OVERFLOW_DISCARD
+    assert env.state.current_player == 0
+
+    actions = env._legal_actions(env.state)
+    env.step(actions[0])
+
+    assert env.state.node_type == NodeType.NOBLE_CLAIM
+    assert env.state.current_player == 0
+
+    actions = env._legal_actions(env.state)
+    env.step(actions[0])
+
+    
+    assert env.state.node_type == NodeType.MAIN_DECISION
+    assert env.state.current_player == 1
 
 # TODO need a regular discard test case 
 # TODO need a test case where you bought a card and activates 2-3 noble claim
